2073b6da62a3ca0edd9c4aea4445a8b12381ca4e
AGoodPuppy <182694132+AGoodPuppy@users.noreply.github.com>
2024-09-26 13:24:19 +0530

    Basic loading and playing of selected ogg file on clicking "Begin Hypnosis"

---
+++ Scripts/select_and_play_file.gd
@@ -0,0 +1,37 @@
+extends FileDialog
+
+var selectedfile
+var ASPlayer2D: AudioStreamPlayer2D
+var AL2D: AudioListener2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	ASPlayer2D = $"../ASPlayer2D"
+	
+	popup()
+	
+	pass # Replace with function body.
+
+func _on_FileDialog_file_selected(path:String):
+	print(path)
+	print(path.right(3))
+	print(path.right(3).to_lower())
+	match path.right(3).to_lower():
+		"ogg":
+			ASPlayer2D.stream = AudioStreamOggVorbis.load_from_file(path)
+		"mp3":
+			print("MP3 importing not implemented yet")
+		_:
+			print("unexpected file type")
+	ASPlayer2D.play()
+	
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_play_finished() -> void:
+	var Error = get_tree().change_scene_to_file("res://Scenes/FirstScene.tscn")
+	if Error:
+		print("Error %d when changing scene." % Error)
