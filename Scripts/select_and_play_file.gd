9fc33602c61983cca21f75c644b30f89d6a173f0
AGoodPuppy <182694132+AGoodPuppy@users.noreply.github.com>
2024-10-05 17:59:09 +0530

    Major restructuring
    - Loading and reloading of hypno scene
    Meeting spec:
    - Added a Load button instead of auto launching the popup file dialog.
    - loading and displaying the loaded file.
    - Manual begin hypnosis
    - Transitioning back to main menu
    - Multiple plays allowed with the same file without reselecting the audio file

--- Scripts/select_and_play_file.gd
+++ Scripts/select_and_play_file.gd
@@ -1,15 +1,11 @@
 extends FileDialog
 
 var selectedfile
-var ASPlayer2D: AudioStreamPlayer2D
-var AL2D: AudioListener2D
+var Main:Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	ASPlayer2D = $"../ASPlayer2D"
-	
-	popup()
-	
+	Main = $"../.."
 	pass # Replace with function body.
 
 func _on_FileDialog_file_selected(path:String):
@@ -17,24 +13,12 @@ func _on_FileDialog_file_selected(path:String):
 	print(path.right(3))
 	print(path.right(3).to_lower())
 	match path.right(3).to_lower():
-		"ogg":
-			ASPlayer2D.stream = AudioStreamOggVorbis.load_from_file(path)
-		"mp3":
-			var file = FileAccess.open(path, FileAccess.READ)
-			var mp3 = AudioStreamMP3.new()
-			mp3.data = file.get_buffer(file.get_length())
-			ASPlayer2D.stream = mp3
+		"ogg", "mp3":
+			Main.setFilePath(path)
 		_:
 			print("unexpected file type")
-	ASPlayer2D.play()
 	
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
 
-
-func _on_play_finished() -> void:
-	var Error = get_tree().change_scene_to_file("res://Scenes/FirstScene.tscn")
-	if Error:
-		print("Error %d when changing scene." % Error)
+func _on_load_file_pressed():
+	popup()
